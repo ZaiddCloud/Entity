beb844ff01fe0775da18ecfd2d31c54a74e06bb8
ZaiddCloud <hsnm774330213@gmail.com>
2026-01-08 22:22:53 +0300

    Phase 3: Migrated ManuscriptViewer to Technologies/Manuscripter

--- resources/js/Pages/Editor/EditorPage.vue
+++ resources/js/Pages/Editor/EditorPage.vue
@@ -38,7 +38,7 @@ const store = useEditorStore()
 const editorRef = ref(null)
 
 // Static imports for stability in tests and simple view
-import ManuscriptViewer from './Components/Viewers/ManuscriptViewer.vue'
+import ManuscriptViewer from '@/Technologies/Manuscripter/ManuscriptViewer.vue'
 import MediaPlayer from '@/Technologies/Player/MediaPlayer.vue'
 import AudioSegmentEditor from './Components/Content/AudioSegmentEditor.vue'
 import VideoSceneEditor from './Components/Content/VideoSceneEditor.vue'
--- resources/js/Technologies/Manuscripter/ManuscriptViewer.vue
+++ resources/js/Technologies/Manuscripter/ManuscriptViewer.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, computed, watch, onUnmounted } from 'vue'
-import ResourceNavigator from '../Common/ResourceNavigator.vue'
+import ResourceNavigator from '@/Pages/Editor/Components/Common/ResourceNavigator.vue'
 
 defineOptions({
   name: 'ManuscriptViewer'
